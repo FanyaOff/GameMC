37cc8de8b17ab04eeb01c7c0ecba2fccb5aa3710
Fanya <ifunnyi1@yandex.ru>
2025-10-27 00:25:55 +0300

    moved game selection menu to keybind

--- src/main/java/com/fanya/gamemc/GameMC.java
+++ src/main/java/com/fanya/gamemc/GameMC.java
@@ -1,6 +1,12 @@
 package com.fanya.gamemc;
 
+import com.fanya.gamemc.screen.GameSelectionScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,8 +14,25 @@ public class GameMC implements ClientModInitializer {
     public static final String MOD_ID = "gamemc";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    private static KeyBinding openScreenKey;
+
     @Override
     public void onInitializeClient() {
         LOGGER.info("GameMC initialized!");
+
+        openScreenKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.gamemc.open_screen",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_HOME,
+                "category.gamemc.keys"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openScreenKey.wasPressed()) {
+                if (client.player != null) {
+                    client.execute(() -> client.setScreen(new GameSelectionScreen(client.currentScreen)));
+                }
+            }
+        });
     }
 }
